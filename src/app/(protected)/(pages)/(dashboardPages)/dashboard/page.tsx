c9687d43d56866f5c68682303806a58e44aba189
AbdulTawabJuly <tawabmasood31july@gmail.com>
2025-03-12 01:08:11 +0500

    very small change

--- src/app/(protected)/(pages)/(dashboardPages)/dashboard/page.tsx
+++ src/app/(protected)/(pages)/(dashboardPages)/dashboard/page.tsx
@@ -8,7 +8,7 @@ const DashboardPage = async () => {
   return (
     <div className="w-full flex flex-col gap-6 relative md:p-0 p-4">
       <div className=" flex flex-col-reverse items-start w-full gap-6 sm:flex-row sm:justify-between sm:items-center">
-        <div className=" flex flex-col items-start ">
+        <div className=" flex flex-col items-start ml-2">
           <h1 className="text-2xl font-semibold dark:text-primary backdrop-blur-lg">
             Projects
           </h1>
