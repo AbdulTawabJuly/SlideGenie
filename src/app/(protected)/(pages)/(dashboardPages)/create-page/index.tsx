663a744430ee5fbe7c3dd9873091489ecfa95814
AbdulTawabJuly <tawabmasood31july@gmail.com>
2025-03-13 02:18:49 +0500

    added and created create page skeleton component

--- src/app/(protected)/(pages)/(dashboardPages)/create-page/index.tsx
+++ src/app/(protected)/(pages)/(dashboardPages)/create-page/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React ,{Suspense} from 'react'
+import CreatePageSkeleton from './_components/CreatePage/CreatePageSkeleton'
 
 type Props = {}
 
 const index = (props: Props) => {
   return (
-    <div>index</div>
+    <main className='w-full h-full pt-6'>
+      <Suspense fallback={<CreatePageSkeleton/>}>
+
+      </Suspense> 
+    </main>
   )
 }
 
