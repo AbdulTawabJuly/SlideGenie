f4e2b54a1be25ac2064b38f194a699b84e168014
AbdulTawabJuly <tawabmasood31july@gmail.com>
2025-03-14 03:04:25 +0500

    added props for card list function

--- src/app/(protected)/(pages)/(dashboardPages)/create-page/_components/Common/CardList.tsx
+++ src/app/(protected)/(pages)/(dashboardPages)/create-page/_components/Common/CardList.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { OutlineCard } from "@/lib/types";
+import React from "react";
 
-type Props = {}
+type Props = {
+  outlines: OutlineCard[];
+  editingCard: string | null;
+  selectedCard: string | null;
+  editText: string;
+  addOutline?: (card: OutlineCard) => void;
+  onEditChange: (value: string) => void;
+  onCardSelected: (id: string) => void;
+  onCardDoubleClick: (id: string, title: string) => void;
+  setEditText: (value: string) => void;
+  setEditingCard: (id: string | null) => void;
+  setSelectedCard: (id: string | null) => void;
+  addMultipleOutlines: (card: OutlineCard[]) => void;
+};
 
 const CardList = (props: Props) => {
-  return (
-    <div>CardList</div>
-  )
-}
+  return <div>CardList</div>;
+};
 
-export default CardList
+export default CardList;
