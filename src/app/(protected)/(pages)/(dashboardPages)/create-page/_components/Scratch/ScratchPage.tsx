e3a7bb6ca8f233033954494dabecae95577e3fc8
AbdulTawabJuly <tawabmasood31july@gmail.com>
2025-03-15 02:55:20 +0500

    added Scratch Page component

---
+++ src/app/(protected)/(pages)/(dashboardPages)/create-page/_components/Scratch/ScratchPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+    onBack : () => void
+}
+
+const ScratchPage = ({onBack}: Props) => {
+  return (
+    <div>ScratchPage</div>
+  )
+}
+
+export default ScratchPage
