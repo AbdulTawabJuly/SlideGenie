b07d1cfda099433ddbf62e1a798ef87d3dd009f7
AbdulTawabJuly <tawabmasood31july@gmail.com>
2025-03-16 00:20:16 +0500

    presentation page part 1

--- src/app/(protected)/presentation/[presentationId]/page.tsx
+++ src/app/(protected)/presentation/[presentationId]/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { getProjectById } from "@/actions/project";
+import { themes } from "@/lib/constants";
 import { useSlideStore } from "@/store/useSlideStore";
+import { Loader2 } from "lucide-react";
 import { useTheme } from "next-themes";
-import { useParams } from "next/navigation";
+import { redirect, useParams } from "next/navigation";
 import React, { useEffect } from "react";
+import { toast } from "sonner";
 
 type Props = {};
 
@@ -14,16 +17,41 @@ const Page = (props: Props) => {
   const { setSlides, setProject, setCurrentTheme, currentTheme } =
     useSlideStore();
 
-    useEffect(()   =>{
-        (async () => {
-            try {
-                const res = await getProjectById(params.presentationId as string);
-            } catch (error) {
-                
-            }
-        })()
-    })
-    
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await getProjectById(params.presentationId as string);
+        if (res.status !== 200 || !res.data) {
+          toast.error("Error", {
+            description: "Unable to retrieve Project",
+          });
+          redirect("/dashboard");
+        }
+
+        const findTheme = themes.find(
+          (theme) => theme.name === res.data.themeName
+        );
+        setCurrentTheme(findTheme || themes[0]);
+        setTheme(findTheme?.type === "dark" ? "dark" : "light");
+        setProject(res.data);
+        setSlides(JSON.parse(JSON.stringify(res.data.slides)));
+      } catch (error) {
+        toast.error("Error", {
+          description: "An Unexpected Error Occured",
+        });
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Loader2 className="w-8 h-8 animate-spin text-primary" />
+      </div>
+    );
+  }
 
   return <div>Page</div>;
 };
