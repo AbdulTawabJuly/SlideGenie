7e0476d80a101d7e7ed655fa34b7303d8bd18063
AbdulTawabJuly <tawabmasood31july@gmail.com>
2025-04-15 02:21:57 +0500

    completed layout preview for now and added editor file

--- src/app/(protected)/presentation/[presentationId]/_components/editor-sidebar/leftsidebar/LayoutPreview.tsx
+++ src/app/(protected)/presentation/[presentationId]/_components/editor-sidebar/leftsidebar/LayoutPreview.tsx
@@ -1,13 +1,50 @@
-import { useSlideStore } from '@/store/useSlideStore'
-import React from 'react'
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useSlideStore } from "@/store/useSlideStore";
+import React, { useEffect, useState } from "react";
 
-type Props = {}
+type Props = {};
 
 const LayoutPreview = (props: Props) => {
-    const {} = useSlideStore()
-  return (
-    <div>LayoutPreview</div>
-  )
-}
+  const { getOrderedSlides, reorderSlides } = useSlideStore();
+  const slides = getOrderedSlides();
+  const [loading, setLoading] = useState(true);
 
-export default LayoutPreview
+  useEffect(() => {
+    if (typeof window !== "undefined") setLoading(false);
+  }, []);
+
+  return (
+    <div className="w-64 h-full fixed left-0 top-20 border-r overscroll-y-auto">
+      <ScrollArea className="h-full w-full" suppressHydrationWarning>
+        {loading ? (
+          <div className="w-full px-4 flex flex-col space-y-6">
+            <Skeleton className="w-full h-20" />
+            <Skeleton className="w-full h-20" />
+            <Skeleton className="w-full h-20" />
+          </div>
+        ) : (
+          <div className="p-4 pb-32 space-y-6">
+            <div className="flex items-center justify-between mb-6">
+              <h2 className="text-sm font-medium dark:text-gray-100 text-gray-500">
+                Slides
+              </h2>
+              <span
+                className="text-xs dark:text-gray-200 text-gray-400"
+                suppressHydrationWarning
+              >
+                {slides?.length} Slides
+              </span>
+            </div>
+            {/* {slides.map((slide,index )=>(
+                <DraggableSlidePreview key={slide.id || index} slide = {slide} index={index}  moveSlide={moveSlide}/>
+                
+            ))} */}
+          </div>
+        )}
+      </ScrollArea>
+    </div>
+  );
+};
+
+export default LayoutPreview;
