02b2a4f1ca3a8b678e63202e85e5344e888d5707
AbdulTawabJuly <tawabmasood31july@gmail.com>
2025-04-16 02:39:35 +0500

    editor part 5

---
+++ src/app/(protected)/presentation/[presentationId]/_components/editor/MasterRecursiveComponent.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { Heading1 } from "@/components/global/editor/components/Headings";
+import { ContentItem } from "@/lib/types";
+import { motion } from "framer-motion";
+import React, { useCallback } from "react";
+
+type MasterRecursiveComponentProps = {
+  content: ContentItem;
+  onContentChange: (
+    contentId: string,
+    newContent: string | string[] | string[][]
+  ) => void;
+  isPreview?: boolean;
+  isEditable?: boolean;
+  slideId: string;
+  index?: number;
+};
+
+const ContentRenderer: React.FC<MasterRecursiveComponentProps> = React.memo(
+  ({ content, onContentChange, slideId, index, isPreview }) => {
+    const handleChange = useCallback(
+      (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        onContentChange(content.id, e.target.value);
+      },
+      [content.id, onContentChange]
+    );
+
+    const commonProps = {
+      placeHolder: content.placeholder,
+      value: content.content as string,
+      onChange: handleChange,
+      isPreview: isPreview,
+    };
+
+    const animationProps = {
+      initial: { opacity: 0, y: 20 },
+      animate: { opacity: 1, y: 0 },
+      transition: { duration: 0.5 },
+    };
+
+    switch (content.type) {
+      case "heading1":
+        return (
+          <motion.div className="w-full h-full">
+            <Heading1 {...commonProps} />
+          </motion.div>
+        );
+        default:
+            return null;
+    }
+  }
+);
---
+++ src/components/global/editor/components/Headings.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+interface HeadingProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  className?: string;
+  styles?: React.CSSProperties;
+  isPreview?: boolean;
+}
+
+const createHeading = (displayName: string, defaultClassName: string) => {
+  const Heading = React.forwardRef<HTMLTextAreaElement, HeadingProps>(
+    ({ children, styles, isPreview = false, ...prop }, ref) => {}
+  );
+};
+const Heading1 = createHeading("Heading1", "text-4xl");
+
+export { Heading1 };
