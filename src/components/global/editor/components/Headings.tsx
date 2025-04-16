0aa1f8faada18fef9b08059a2ca2c20cdbeb4394
AbdulTawabJuly <tawabmasood31july@gmail.com>
2025-04-16 22:51:42 +0500

    temp

--- src/components/global/editor/components/Headings.tsx
+++ src/components/global/editor/components/Headings.tsx
@@ -1,6 +1,6 @@
 "use client";
+import React, { useEffect, useRef } from "react";
 
-import React from "react";
 interface HeadingProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   className?: string;
@@ -10,7 +10,21 @@ interface HeadingProps
 
 const createHeading = (displayName: string, defaultClassName: string) => {
   const Heading = React.forwardRef<HTMLTextAreaElement, HeadingProps>(
-    ({ children, styles, isPreview = false, ...prop }, ref) => {}
+    ({ children, styles, isPreview = false, ...prop }, ref) => {
+      const textAreaRef = useRef<HTMLTextAreaElement>(null);
+      useEffect(() => {
+        const textarea = textAreaRef.current;
+        if (textarea && !isPreview) {
+          const adjustHeight = () => {
+            textarea.style.height = "0";
+            textarea.style.height = `${textarea.scrollHeight}px`;
+          };
+          textarea.addEventListener("input", adjustHeight);
+          adjustHeight();
+          return () => textarea.removeEventListener("input", adjustHeight);
+        }
+      }, [isPreview]);
+    }
   );
 };
 const Heading1 = createHeading("Heading1", "text-4xl");
