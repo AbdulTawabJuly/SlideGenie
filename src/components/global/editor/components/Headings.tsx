73dbf74d7b07493c177a4c25715494082b427e91
AbdulTawabJuly <tawabmasood31july@gmail.com>
2025-04-20 13:34:25 +0500

    createHeading function complete

--- src/components/global/editor/components/Headings.tsx
+++ src/components/global/editor/components/Headings.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useEffect, useRef } from "react";
+import { cn } from "@/lib/utils";
 
 interface HeadingProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
@@ -10,7 +11,7 @@ interface HeadingProps
 
 const createHeading = (displayName: string, defaultClassName: string) => {
   const Heading = React.forwardRef<HTMLTextAreaElement, HeadingProps>(
-    ({ children, styles, isPreview = false, ...prop }, ref) => {
+    ({ children, styles, isPreview = false, className, ...props }, ref) => {
       const textAreaRef = useRef<HTMLTextAreaElement>(null);
       useEffect(() => {
         const textarea = textAreaRef.current;
@@ -24,9 +25,41 @@ const createHeading = (displayName: string, defaultClassName: string) => {
           return () => textarea.removeEventListener("input", adjustHeight);
         }
       }, [isPreview]);
+
+      const previewClassName = isPreview ? "text-xs" : "";
+
+      return (
+        <textarea
+          className={cn(
+            `w-full bg-transparent ${defaultClassName} ${previewClassName} font-normal text-gray-900 placeholder:text-gray-300 focus:outline-none resize-none overflow-hidden leading-tight`,
+            className
+          )}
+          style={{
+            padding: 0,
+            margin: 0,
+            color: "inherit",
+            boxSizing: "content-box",
+            lineHeight: "1.2em",
+            minHeight: "1.2em",
+            ...styles,
+          }}
+          ref={(el) => {
+            (textAreaRef.current as HTMLTextAreaElement | null) = el;
+            if (typeof ref === "function") ref(el);
+            else if (ref) ref.current = el;
+          }}
+          readOnly={isPreview}
+        ></textarea>
+      );
     }
   );
+  Heading.displayName = displayName;
+  return Heading;
 };
 const Heading1 = createHeading("Heading1", "text-4xl");
+const Heading2 = createHeading("Heading2", "text-3xl");
+const Heading3 = createHeading("Heading3", "text-2xl");
+const Heading4 = createHeading("Heading4", "text-xl");
+const Title = createHeading("Title", "text-5xl");
 
-export { Heading1 };
+export { Heading1, Heading2, Heading3, Heading4, Title };
