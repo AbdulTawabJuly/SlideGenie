7c7e4618a98eb3c318c2eb770cf044548f802a74
AbdulTawabJuly <abdul.tawab@ripeseed.io>
2025-06-25 00:21:50 +0500

    Part 4

--- src/app/(protected)/(pages)/(dashboardPages)/dashboard/page.tsx
+++ src/app/(protected)/(pages)/(dashboardPages)/dashboard/page.tsx
@@ -1,13 +1,12 @@
-// import { getAllProjects } from "@/actions/project";
+import { getAllProjects } from "@/actions/project";
 import React from "react";
-// import DashboardClient from "./DashboardClient";
+import DashboardClient from "./DashboardClient";
 
 const DashboardPage = async () => {
-  // const allProjects = await getAllProjects();
+  const allProjects = await getAllProjects();
   
   return (
-    // <DashboardClient projects={allProjects.data || []} />
-    <h1>Dashboard Page is working fine</h1>
+    <DashboardClient projects={allProjects.data || []} />
   );
 };
 
